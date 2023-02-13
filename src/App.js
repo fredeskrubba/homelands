345f7e4069fa941c9e879d0be51fd303775315e1
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2023-02-13 10:43:34 +0100

    make footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Route } from "wouter";
 import Home from "./pages/Home";
 import Listings from "./pages/Listings";
 import Login from "./pages/Login";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-
+      <Footer/>
       <Route path="/" component={Home} />
       <Route path="/listings" component={Listings} />
       <Route path="/Login" component={Login} />
---
+++ src/components/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useGlobalStore } from '../stores/globalStore'
+
+const FooterDiv = styled.footer`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: ${props => props.background};
+    color: white;
+    padding: 1.5vw 6vw;
+    .info-section{
+        display: flex;
+        align-items: center;
+        gap: 4vw;
+        p{
+            margin: 0;
+        }
+        h2{
+            font-size: 2.5vw;
+            margin: 0;
+        }
+    }
+`
+
+const Footer = () => {
+    const colors = useGlobalStore((state)=> state.colors)
+  return (
+    <FooterDiv background={colors.black}>
+        <div className='info-section'>
+            <h2>Homelands</h2>
+            <section>
+                <p>Øster Uttrupvej 5</p>
+                <p>9000 Aalborg</p> 
+            </section>
+            <section>
+                <p>Email: info@homelands.dk</p>
+                <p>Telefon: +45 1122 3344 </p>
+            </section>
+
+        </div>
+    </FooterDiv>
+  )
+}
+
+export default Footer
